b0e1aa2d39a740f35b783dace28c6f8b318e455a
محمود محمَّد سعيد <49478350+Flying-Raiden@users.noreply.github.com>
2024-05-23 04:32:39 +0300

    Attempt to solve day1 part2

--- src/Day1.java
+++ src/Day1.java
@@ -1,17 +1,21 @@
 
+import java.util.Hashtable;
+import java.util.Map;
+
 public class Day1 {
+
     public int part1(String[] str_arr) {
         int sum = 0;
-        for(String line : str_arr){
+        for (String line : str_arr) {
 
             int firstdigit = -1;
             int lastdigit = 0;
 
-            for(int i =0 ;i < line.length();i++){
+            for (int i = 0; i < line.length(); i++) {
 
                 char c = line.charAt(i);
 
-                if( c >= '0' && c <= '9') {
+                if (c >= '0' && c <= '9') {
                     lastdigit = Character.getNumericValue(c);
                     if (firstdigit == -1) {
                         firstdigit = Character.getNumericValue(c);
@@ -19,8 +23,70 @@ public class Day1 {
                 }
 
             }
-            sum += firstdigit*10 + lastdigit;
+            sum += firstdigit * 10 + lastdigit;
         }
-    return sum;
+        return sum;
+    }
+
+    public int part2(String[] str_arr) {
+        Map<String, Integer> dict = new Hashtable<>() {
+            {
+                put("one", 1);
+                put("two", 2);
+                put("three", 3);
+                put("four", 4);
+                put("five", 5);
+                put("six", 6);
+                put("seven", 7);
+                put("eight", 8);
+                put("nine", 9);
+            }
+        };
+
+        int sum = 0;
+        for (String line : str_arr) {
+
+            int firstdigit = -1;
+            int lastdigit = 0;
+
+            for (int i = 0; i < line.length(); i++) {
+
+                char c = line.charAt(i);
+
+                if (c >= '0' && c <= '9') {
+                    lastdigit = Character.getNumericValue(c);
+                    if (firstdigit == -1) {
+                        firstdigit = Character.getNumericValue(c);
+                    }
+                } else if (i + 2 < line.length()) {
+                    String str = line.substring(i, i + 2);
+                    if (dict.containsKey(str)) {
+                        lastdigit = dict.get(str);
+                        if (firstdigit == -1) {
+                            firstdigit = dict.get(str);
+                        }
+                    }
+                } else if (i + 3 < line.length()) {
+                    String str = line.substring(i, i + 3);
+                    if (dict.containsKey(str)) {
+                        lastdigit = dict.get(str);
+                        if (firstdigit == -1) {
+                            firstdigit = dict.get(str);
+                        }
+                    }
+                } else if (i + 4 < line.length()) {
+                    String str = line.substring(i, i + 4);
+                    if (dict.containsKey(str)) {
+                        lastdigit = dict.get(str);
+                        if (firstdigit == -1) {
+                            firstdigit = dict.get(str);
+                        }
+                    }
+                }
+            }
+            sum += firstdigit * 10 + lastdigit;
+        }
+        return sum;
+
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,18 @@
+
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.io.IOException;
 import java.util.List;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
 public class Main {
+
     public static void main(String[] args) {
         String filePath = "./input.txt";
         try {
             // Read all lines from the file
             List<String> lines = Files.readAllLines(Paths.get(filePath));
-
 
             String[] linesArray = lines.toArray(new String[0]);
             // Convert the List to an Array
@@ -18,10 +20,10 @@ public class Main {
                 System.out.println(line);
             }
             Day1 d = new Day1();
-            System.out.println(d.part1(linesArray));
+            System.out.println(d.part2(linesArray));
 
         } catch (IOException e) {
             e.printStackTrace();
         }
-        }
     }
+}
